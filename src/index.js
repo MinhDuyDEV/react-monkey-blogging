5153d1987c43d9239633081f0e7ecb264cd3dc98
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2022-12-31 13:34:09 +0700

    setup routes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import { theme } from "./utils/constants";
+import { BrowserRouter } from "react-router-dom";
 
 import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
@@ -14,7 +15,9 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles></GlobalStyles>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
