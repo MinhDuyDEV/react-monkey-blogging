6b0a3155cf75e5bf376f18a45d3230550a767b12
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-01-04 21:04:37 +0700

    create and optimize component PostImage

--- src/module/post/PostItem.js
+++ src/module/post/PostItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PostCategory from "./PostCategory";
+import PostImage from "./PostImage";
 import PostMeta from "./PostMeta";
 import PostTitle from "./PostTitle";
 const PostItemStyles = styled.div`
@@ -11,14 +12,8 @@ const PostItemStyles = styled.div`
     &-image {
       height: 202px;
       margin-bottom: 20px;
-      display: block;
       width: 100%;
-      img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        border-radius: 16px;
-      }
+      border-radius: 16px;
     }
     &-category {
       margin-bottom: 16px;
@@ -32,12 +27,10 @@ const PostItemStyles = styled.div`
 const PostItem = () => {
   return (
     <PostItemStyles>
-      <div className="post-image">
-        <img
-          src="https://images.unsplash.com/photo-1570993492881-25240ce854f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2290&q=80"
-          alt=""
-        />
-      </div>
+      <PostImage
+        url="https://images.unsplash.com/photo-1570993492881-25240ce854f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2290&q=80"
+        alt="unsplash"
+      ></PostImage>
       <PostCategory>Kiến thức</PostCategory>
       <PostTitle size="normal">
         Hướng dẫn setup phòng cực chill dành cho người mới toàn tập
