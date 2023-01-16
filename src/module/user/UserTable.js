19861675d47f238caf68c5fb9263c5b7af8bf117
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-01-16 09:49:41 +0700

    update delete user

--- src/module/user/UserTable.js
+++ src/module/user/UserTable.js
@@ -25,8 +25,8 @@ const UserTable = () => {
       setUserList(results);
     });
   }, []);
-  const handleDeleteUser = async (user) => {
-    const colRef = doc(db, "users", user.id);
+  const handleDeleteUser = async (id) => {
+    const colRef = doc(db, "users", id);
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
