b0dc9632e87b280e330db344f365e7643ee3eff7
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-01-17 00:49:47 +0700

    filter post, load more post

--- src/module/post/PostManage.js
+++ src/module/post/PostManage.js
@@ -1,10 +1,106 @@
-import React from "react";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  limit,
+  onSnapshot,
+  query,
+  startAfter,
+  where,
+} from "firebase/firestore";
+import { debounce } from "lodash";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
+import { ActionDelete, ActionEdit, ActionView } from "../../components/action";
 import { Button } from "../../components/button";
 import { Dropdown } from "../../components/dropdown";
+import { Table } from "../../components/table";
+import { db } from "../../firebase/firebase-config";
 import DashboardHeading from "../dashboard/DashboardHeading";
-import PostTable from "./PostTable";
+
+const POST_PER_PAGE = 1;
 
 const PostManage = () => {
+  const navigate = useNavigate();
+  const [postList, setPostList] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [total, setTotal] = useState(0);
+  const [lastDoc, setLastDoc] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const colRef = collection(db, "posts");
+      const newRef = filter
+        ? query(
+            colRef,
+            where("title", ">=", filter),
+            where("title", "<=", filter + "utf8")
+          )
+        : query(colRef, limit(POST_PER_PAGE));
+      const documentSnapshot = await getDocs(newRef);
+      const lastVisible =
+        documentSnapshot.docs[documentSnapshot.docs.length - 1];
+      onSnapshot(colRef, (snapshot) => {
+        setTotal(snapshot.size);
+      });
+      onSnapshot(newRef, (snapshot) => {
+        const results = [];
+        snapshot.forEach((post) => {
+          results.push({
+            id: post.id,
+            ...post.data(),
+          });
+        });
+        setPostList(results);
+      });
+      setLastDoc(lastVisible);
+    }
+    fetchData();
+  }, [filter]);
+  const handleDeletePost = async (postId) => {
+    const colRef = doc(db, "posts", postId);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await deleteDoc(colRef);
+        toast.success("Delete user successfully");
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
+  };
+  const handleLoadMorePost = async () => {
+    const nextRef = query(
+      collection(db, "posts"),
+      startAfter(lastDoc),
+      limit(POST_PER_PAGE)
+    );
+    onSnapshot(nextRef, (snapshot) => {
+      let results = [];
+      snapshot.forEach((post) => {
+        results.push({
+          id: post.id,
+          ...post.data(),
+        });
+      });
+      setPostList([...postList, ...results]);
+    });
+    const documentSnapshot = await getDocs(nextRef);
+    const lastVisible = documentSnapshot.docs[documentSnapshot.docs.length - 1];
+    setLastDoc(lastVisible);
+  };
+  const handleInputFilter = debounce((e) => {
+    setFilter(e.target.value);
+  }, 500);
+
   return (
     <div>
       <DashboardHeading
@@ -22,15 +118,92 @@ const PostManage = () => {
             type="text"
             className="w-full p-4 border border-gray-300 border-solid rounded-lg"
             placeholder="Search post..."
+            onChange={handleInputFilter}
           />
         </div>
       </div>
-      <PostTable></PostTable>
+      <Table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Post</th>
+            <th>Category</th>
+            <th>Author</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {postList.length > 0 &&
+            postList.map((post) => {
+              const date = post?.createdAt.seconds
+                ? new Date(post?.createdAt.seconds * 1000)
+                : new Date();
+              const formatDate = new Date(date).toLocaleDateString("vi-VI");
+              return (
+                <tr key={post.id}>
+                  <td title={post.id}>{post?.id.slice(0, 5) + "..."}</td>
+                  <td>
+                    <div className="flex items-center gap-x-3">
+                      <img
+                        src={post?.image}
+                        alt=""
+                        className="w-[66px] h-[55px] rounded object-cover"
+                      />
+                      <div className="flex-1">
+                        <h3
+                          className="font-semibold"
+                          title={post?.title.length > 50 ? post?.title : null}
+                        >
+                          {post?.title.length > 50
+                            ? post?.title.slice(0, 50) + "..."
+                            : post?.title}
+                        </h3>
+                        <time className="text-sm text-gray-500">
+                          Date: {formatDate}
+                        </time>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <span className="text-gray-500">
+                      {post?.category?.name}
+                    </span>
+                  </td>
+                  <td>
+                    <span className="text-gray-500">
+                      {post?.user?.username}
+                    </span>
+                  </td>
+                  <td>
+                    <div className="flex items-center gap-x-3">
+                      <ActionView
+                        onClick={() => navigate(`/${post?.slug}`)}
+                      ></ActionView>
+                      <ActionEdit
+                        onClick={() =>
+                          navigate(`/manage/update-post?id=${post?.id}`)
+                        }
+                      ></ActionEdit>
+                      <ActionDelete
+                        onClick={() => handleDeletePost(post?.id)}
+                      ></ActionDelete>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
       <div className="mt-10 text-center">
-        {/* <Pagination></Pagination> */}
-        <Button kind="ghost" className="mx-auto w-[200px]">
-          See more+
-        </Button>
+        {total > postList.length && (
+          <Button
+            kind="ghost"
+            className="mx-auto w-[200px]"
+            onClick={handleLoadMorePost}
+          >
+            See more+
+          </Button>
+        )}
       </div>
     </div>
   );
